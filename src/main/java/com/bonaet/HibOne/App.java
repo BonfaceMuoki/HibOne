f8014d91e9064846af7dea9f2c36af8fc8d8ae2e
Bonface Muoki <bonface@yieldexchange.ca>
2025-02-07 22:18:14 +0300

    adding mysql and hibernate and saving data to DB

--- src/main/java/com/bonaet/HibOne/App.java
+++ src/main/java/com/bonaet/HibOne/App.java
@@ -1,16 +1,38 @@
 package com.bonaet.HibOne;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App  //POJO
+public class App // POJO
 {
-    public static void main( String[] args )
-    {
-    	User u = new User();
-    	u.setFname("Bonface");
-    	u.setLname("Muoki");    	
-        
-    }
+	public static void main(String[] args) {
+		User u = new User();
+		u.setFname("Bonface");
+		u.setLname("Muoki");
+
+		Configuration conn = new Configuration().addAnnotatedClass(User.class);
+    	conn.configure("hibernate.cfg.xml");
+		SessionFactory sf = conn.buildSessionFactory();
+		Session s = sf.openSession();
+		Transaction tx = s.beginTransaction();
+		try {
+			
+			s.save(u);
+			tx.commit();
+			
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			s.close();
+		}
+
+	}
 }
--- src/main/java/com/bonaet/HibOne/User.java
+++ src/main/java/com/bonaet/HibOne/User.java
@@ -1,6 +1,15 @@
 package com.bonaet.HibOne;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+	 @Id	 
+	 @GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private int id;
 	private String Fname;
 	private String Lname;
 
